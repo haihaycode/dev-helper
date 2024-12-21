e474333fcd9c3272dfcd9914f9b0744ffb966df1
haihaycode <kienthuc177@gmail.com>
2024-12-21 11:49:32 +0700

    fix header

--- src/components/layout/HeaderComponet.vue
+++ src/components/layout/HeaderComponet.vue
@@ -1,83 +1,70 @@
+<!-- eslint-disable-next-line -->
 <template>
-  <AntHeader class="header">
+  <a-layout-header class="header">
     <div class="header-container">
       <!-- Logo -->
       <div class="logo" v-t="'project.name'"></div>
       <!-- Desktop Menu -->
-      <AntMenu
+      <a-menu
         mode="horizontal"
         theme="dark"
         class="desktop-menu"
         :default-selected-keys="['home']"
       >
-        <AntMenuItem key="home">
+        <a-menu-item key="home">
           <router-link to="/" v-t="'nav.home'"></router-link>
-        </AntMenuItem>
-        <AntMenuItem key="about">
+        </a-menu-item>
+        <a-menu-item key="about">
           <router-link to="/about" v-t="'nav.about'"></router-link>
-        </AntMenuItem>
-        <AntMenuItem key="tool">
+        </a-menu-item>
+        <a-menu-item key="tool">
           <router-link to="/tool" v-t="'nav.tool'"></router-link>
-        </AntMenuItem>
-        <AntMenuItem key="contact">
+        </a-menu-item>
+        <a-menu-item key="contact">
           <router-link to="/contact" v-t="'nav.contact'"></router-link>
-        </AntMenuItem>
-      </AntMenu>
+        </a-menu-item>
+      </a-menu>
       <!-- Mobile Menu Toggle Button -->
-      <AntButton
+      <a-button
         class="mobile-menu-button"
         type="text"
         @click="showDrawer = true"
       >
         <MenuOutlined />
-      </AntButton>
+      </a-button>
       <!-- Mobile Drawer Menu -->
-      <AntDrawer
+      <a-drawer
         placement="right"
         closable
         :visible="showDrawer"
         @close="showDrawer = false"
       >
-        <AntMenu
-          mode="vertical"
-          theme="light"
-          :default-selected-keys="['home']"
-        >
-          <AntMenuItem key="home">
+        <a-menu mode="vertical" theme="light" :default-selected-keys="['home']">
+          <a-menu-item key="home">
             <router-link to="/" v-t="'nav.home'"></router-link>
-          </AntMenuItem>
-          <AntMenuItem key="about">
+          </a-menu-item>
+          <a-menu-item key="about">
             <router-link to="/about" v-t="'nav.about'"></router-link>
-          </AntMenuItem>
-          <AntMenuItem key="tool">
+          </a-menu-item>
+          <a-menu-item key="tool">
             <router-link to="/tool" v-t="'nav.tool'"></router-link>
-          </AntMenuItem>
-          <AntMenuItem key="contact">
+          </a-menu-item>
+          <a-menu-item key="contact">
             <router-link to="/contact" v-t="'nav.contact'"></router-link>
-          </AntMenuItem>
-        </AntMenu>
-      </AntDrawer>
+          </a-menu-item>
+        </a-menu>
+      </a-drawer>
     </div>
-  </AntHeader>
+  </a-layout-header>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from "vue";
 import { MenuOutlined } from "@ant-design/icons-vue";
-import AntHeader from "@/components/container/AntHeader.vue";
-import AntMenu from "@/components/container/AntMenu.vue";
-import AntMenuItem from "@/components/container/AntMenuItem.vue";
-import AntButton from "@/components/container/AntButton.vue";
-import AntDrawer from "@/components/container/AntDrawer.vue";
 
 export default defineComponent({
   components: {
     MenuOutlined,
-    AntHeader,
-    AntMenu,
-    AntMenuItem,
-    AntButton,
-    AntDrawer,
   },
   setup() {
     const showDrawer = ref(false);
@@ -88,31 +75,33 @@ export default defineComponent({
 </script>
 
 <style scoped lang="stylus">
+
 .header-container
-  display flex
-  align-items center
-  justify-content space-between
-  max-width 1200px
-  margin 0 auto
-  width 100%
+    display flex
+    align-items center
+    justify-content space-between
+    max-width 1200px
+    margin 0 auto
+    width 100%
+    padding 0 20px
 
 .logo
-  color #fff
-  font-size 24px
-  font-weight bold
+    color #fff
+    font-size 24px
+    font-weight bold
 
 .desktop-menu
-  display flex
+    display flex
 
 .mobile-menu-button
-  display none
-  color white
-  font-size 24px
+    display none
+    color white
+    font-size 24px
 
 /* Responsive Styles */
 @media (max-width: 768px)
-  .desktop-menu
-    display none
-  .mobile-menu-button
-    display block
+    .desktop-menu
+        display none
+    .mobile-menu-button
+        display block
 </style>
