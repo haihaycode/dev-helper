f335780c32d6def5752551419e22a16b8f040651
haihaycode <kienthuc177@gmail.com>
2025-01-01 16:15:57 +0700

    secret key encryption tools

--- src/views/pages/tools-pages/encryption-tools/secret-key-encryption-tools/index.vue
+++ src/views/pages/tools-pages/encryption-tools/secret-key-encryption-tools/index.vue
@@ -1,6 +1,25 @@
 <template>
   <div class="main min-h-[100vh] sm:p-2 md:p-4 lg:p-6 xl:p-8">
     <Breadcrumb :items="breadcrumbItems" />
+    <a-transition name="fade" mode="out-in">
+      <div class="content mt-2 px-2" v-if="tool">
+        <a-typography-title :level="3" class="text-center md:text-left">
+          {{ $t(tool.name) }}
+        </a-typography-title>
+        <a-typography-paragraph
+          class="hidden sm:block"
+          style="text-align: left"
+        >
+          {{ $t(tool.description) }}
+        </a-typography-paragraph>
+        <a-typography-paragraph
+          class="sm:hidden block"
+          style="text-align: left"
+        >
+          {{ truncateString($t(tool.description), 200) }}
+        </a-typography-paragraph>
+      </div>
+    </a-transition>
   </div>
 </template>
 
@@ -9,6 +28,8 @@ import { defineComponent, computed } from "vue";
 import Breadcrumb from "@/components/container/Breadcrumb.vue";
 import { HomeOutlined } from "@ant-design/icons-vue";
 import { getToolTypeById, getToolById } from "@/services/tools/toolsService";
+import { useI18n } from "vue-i18n";
+import { truncateString } from "@/utils/StringUtils";
 
 export default defineComponent({
   name: "SecretKeyEncryptionToolsPage",
@@ -16,11 +37,11 @@ export default defineComponent({
     Breadcrumb,
   },
   setup() {
-    const toolId = "1";
-    const toolTypeId = "1";
-
-    const toolType = getToolTypeById(toolTypeId);
+    const { t } = useI18n();
+    const toolId = "1"; // Replace with actual tool ID
     const tool = getToolById(toolId);
+    const toolTypeId = tool?.idtoolsType || "";
+    const toolType = getToolTypeById(toolTypeId);
 
     const breadcrumbItems = computed(() => [
       { href: "/", icon: HomeOutlined },
@@ -29,12 +50,21 @@ export default defineComponent({
     ]);
 
     return {
+      t,
+      tool,
       breadcrumbItems,
+      truncateString,
     };
   },
 });
 </script>
 
 <style scoped>
-/* Add any additional styles here if needed */
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active in <2.1.8 */ {
+  opacity: 0;
+}
 </style>
