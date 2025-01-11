81dfb1507c457b8b6383691949d46f82ebc8cfa5
haihaycode <haippd07906@fpt.edu.vn>
2025-01-11 15:12:00 +0700

    Changes to be committed: 	modified:   src/api/config.ts 	modified:   vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,15 @@ module.exports = defineConfig({
   transpileDependencies: ["ant-design-vue"],
   devServer: {
     port: 4300,
+    server: {
+      proxy: {
+        "/api": {
+          target:
+            "https://ideal-space-carnival-444xxpx4vpp3q4r6-5000.app.github.dev",
+          changeOrigin: true,
+        },
+      },
+    },
     client: {
       overlay: {
         warnings: false,
