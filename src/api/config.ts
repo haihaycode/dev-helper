db76da0a3dfcaa4b777fcd2bdcb07ce1a5d2db75
haihaycode <haippd07906@fpt.edu.vn>
2025-01-11 15:42:19 +0700

    Changes to be committed: 	modified:   src/api/config.ts 	modified:   vue.config.js

--- src/api/config.ts
+++ src/api/config.ts
@@ -2,8 +2,8 @@ export const MAX_TIME_OUT = 120000;
 export const SUCCESS_CODE = 200;
 export const DEV: boolean = process.env.NODE_ENV !== "production";
 export const HOST: string = DEV
-  ? "https://ideal-space-carnival-444xxpx4vpp3q4r6-5000.app.github.dev"
-  : "https://ideal-space-carnival-444xxpx4vpp3q4r6-5000.app.github.dev";
+  ? "https://friendly-train-9vvppgpv5qrc795w-5000.app.github.dev"
+  : "https://friendly-train-9vvppgpv5qrc795w-5000.app.github.dev";
 //ideal-space-carnival-444xxpx4vpp3q4r6.github.dev/
 // export const HOST: string = DEV
 //   ? "http://localhost:5000"
@@ -11,6 +11,6 @@ export const HOST: string = DEV
 
 export const Domain: string = DEV
   ? "localhost:5000"
-  : "http://68.183.238.32:8083";
+  : "https://friendly-train-9vvppgpv5qrc795w-5000.app.github.dev";
 
 export const rapidHost = "rapidapi.com";
