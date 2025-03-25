4c058da22d47ff518d1120d5b3dba72876010abf
haihaycode <haippd07906@fpt.edu.vn>
2025-03-25 16:30:13 +0700

    -- add vocabulary

---
+++ src/components/navigation/NavigateBackComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <div
+    @click="goBack"
+    id="navigationBack"
+    class="text-blue-900 font-bold text-left flex items-center cursor-pointer space-x-1"
+  >
+    <ArrowLeftOutlined /> <span>Quay lại</span>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ArrowLeftOutlined } from "@ant-design/icons-vue";
+import { useRouter } from "vue-router";
+const router = useRouter();
+function goBack() {
+  router.back(); // Go to the previous route
+}
+</script>
---
+++ src/views/pages/learn-english-pages/vocabulary-pages/ListVocabulary-page.vue
@@ -0,0 +1,33 @@
+<template>
+  <LearnEnglishPagePatternLayout :title="$t('nav.l.english.listVocabulary')">
+    <template #content>
+      <div id="listVocabulary">
+        <div class="flex justify-end action">
+          <button
+            class="px-2 bg-blue-900 hover:bg-blue-400 rounded-sm text-white"
+            @click="showModalVoabularyAdd = true"
+          >
+            Add more +
+          </button>
+        </div>
+      </div>
+      <ModalVocabularyAdd
+        v-if="showModalVoabularyAdd"
+        :modelValue="showModalVoabularyAdd"
+        @update:modelValue="showModalVoabularyAdd = $event"
+        @close="showModalVoabularyAdd = false"
+      />
+    </template>
+  </LearnEnglishPagePatternLayout>
+</template>
+<script setup lang="ts">
+import LearnEnglishPagePatternLayout from "../LearnEnglishPagePatternLayout.vue";
+import ModalVocabularyAdd from "@/components/learn-english/vocabulary/ModalVocabularyAdd.vue";
+import { useStore } from "vuex";
+import { computed, ref } from "vue";
+const store = useStore();
+const locale = computed(() => store.getters["locale/locale"]);
+const showModalVoabularyAdd = ref(false);
+
+console.log(locale);
+</script>
