cdeafad7d997d507f56dc2708e6ae7fdb3d3023a
haihaycode <haippd07906@fpt.edu.vn>
2025-04-02 18:39:51 +0700

    On branch dev Your branch is up to date with 'origin/dev'.
    Changes to be committed:
    	modified:   src/api/noteApi.ts
    	modified:   src/components/learn-english/notes/GridNotesComponent.vue
    	modified:   src/views/pages/learn-english-pages/note-pages/ListNote-page.vue

--- src/views/pages/learn-english-pages/note-pages/ListNote-page.vue
+++ src/views/pages/learn-english-pages/note-pages/ListNote-page.vue
@@ -1,15 +1,19 @@
 <template>
   <LearnEnglishPagePatternLayout :title="i18n.global.t('note.title')">
     <template #content>
-      <div id="NOTE-PAGE">
+      <div id="NOTEPAGE">
         <div id="ACTION" class="flex justify-between navigation mb-2"></div>
         <div id="CONTENT">
-          <GridNotesComponent :notes="notesRes?.data?.results" />
+          <GridNotesComponent
+            :notes="notesRes?.data?.results"
+            :loading="loading"
+          />
         </div>
       </div>
     </template>
   </LearnEnglishPagePatternLayout>
 </template>
+
 <script lang="ts" setup>
 import i18n from "@/services/i18n";
 import LearnEnglishPagePatternLayout from "../LearnEnglishPagePatternLayout.vue";
@@ -21,65 +25,71 @@ import {
   PAGE_SIZE_DEFAULT,
   QUERY_DEFAULT,
 } from "@/utils/global";
-import { onMounted, ref, watchEffect } from "vue";
+import { onMounted, ref, onBeforeUnmount } from "vue";
 import { getAll } from "@/api/noteApi";
 import GridNotesComponent from "@/components/learn-english/notes/GridNotesComponent.vue";
 
-const notesRes = ref<INotesResponse>();
+const loading = ref(false);
+const notesRes = ref<INotesResponse>({
+  data: {
+    results: [],
+    page: 1,
+    pageSize: PAGE_SIZE_DEFAULT,
+    totalCount: 0,
+    totalPages: 0,
+  },
+});
 const noteReq = ref<INoteRequest>({
   query: QUERY_DEFAULT,
   page: PAGE_FIRST,
-  pageSize: PAGE_SIZE_DEFAULT + 10,
+  pageSize:
+    window.innerWidth <= 640 ? PAGE_SIZE_DEFAULT - 2 : PAGE_SIZE_DEFAULT + 2,
   is_deleted: IS_DELETED.is_False,
   orderBy: {
     column: ORDER_BY.COLUMN_DEFAULT,
     order: ORDER_BY.ORDER_DEFAULT,
   },
 });
-const loadedPages = ref<{ [key: number]: INotesResponse }>({});
-const handleSearchByQueryKeyword = (value: string) => {
-  noteReq.value.query = value;
-  noteReq.value.page = 1;
-  loadedPages.value = {};
-};
-const handlePageChange = (page: number) => {
-  noteReq.value.page = page;
-  handleFetchNotes();
-};
-
-const handlePageSizeChange = ({
-  current,
-}: {
-  current: { current: number; pageSize: number };
-}) => {
-  noteReq.value.page = current.current;
-  noteReq.value.pageSize = current.pageSize;
-  loadedPages.value = {};
-  handleFetchNotes();
-};
 
 const handleFetchNotes = async () => {
-  const currentPage = noteReq.value.page;
-  if (currentPage !== undefined && loadedPages.value[currentPage]) {
-    notesRes.value = loadedPages.value[currentPage];
-    return;
-  }
+  loading.value = true;
   try {
     const response = await getAll(noteReq.value);
-    notesRes.value = response;
-    if (currentPage !== undefined) {
-      loadedPages.value[currentPage] = response;
+    if (noteReq.value.page === 1) {
+      notesRes.value = response;
+    } else if (response.data?.results && notesRes.value?.data?.results) {
+      notesRes.value.data.results.push(...response.data.results);
     }
   } catch (error) {
     console.error(error);
+  } finally {
+    loading.value = false;
+  }
+};
+
+const handleScroll = () => {
+  if (
+    loading.value ||
+    (noteReq.value.page !== undefined &&
+      notesRes.value.data &&
+      noteReq.value.page >= notesRes.value.data.totalPages)
+  )
+    return;
+  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+  if (scrollHeight - scrollTop <= clientHeight + 5) {
+    if (noteReq.value.page !== undefined) {
+      noteReq.value.page++;
+    }
+    handleFetchNotes();
   }
 };
 
 onMounted(() => {
   handleFetchNotes();
+  window.addEventListener("scroll", handleScroll);
 });
 
-watchEffect(() => {
-  handleFetchNotes();
+onBeforeUnmount(() => {
+  window.removeEventListener("scroll", handleScroll);
 });
 </script>
